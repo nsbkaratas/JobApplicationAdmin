445691cf953b7a5a0cdaf1032a2a86e1b579255b
Nesibe Karatas <nsbkaratas@gmail.com>
2021-10-27 12:20:37 -0400

    Fixing footer

--- job-application-admin/src/components/Footer.jsx
+++ job-application-admin/src/components/Footer.jsx
@@ -1,20 +1,7 @@
-import instagram from "../Images/instagram.png"
-import facebook from "../Images/facebook.png"
-import twitter from "../Images/twitter.png"
-
-
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="footer-content">
-                {/* <h3>Purr-fect Foods</h3> */}
-                <p>Follow us on social media</p>
-                <ul>
-                    <li><a href=""><img src={instagram} alt="instagram" /></a></li>
-                    <li><a href=""><img src={facebook} alt="facebook" /></a></li>
-                    <li><a href=""><img src={twitter} alt="twitter" /></a></li>                
-                </ul>
-            </div>
+          
             <div className="footer-bottom">
                 <p>copyright &copy;{new Date().getFullYear()} Job application management tool - All rights reserved</p>
             </div>
